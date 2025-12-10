67725dfb46fe3f9d2bbe8d6861bccb8004ec4f82
cmzadkine <c.musukwa@hotmail.com>
2025-12-10 14:56:19 +0100

    Add files via upload

---
+++ src/Classes/Calculator.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Classes;
+
+class Calculator
+{
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+
+    public function subtract($a, $b)
+    {
+        return $a - $b;
+    }
+}
